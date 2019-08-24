cdbd44d7c1acc2f084adf2bfa5a35c7c3ee3b70c
khanly101 <khantuanly@gmail.com>
2019-08-24 08:20:13 -0700

    Updates

---
+++ precache-manifest.8e5b4653df8ec27e2135823fb5a274d7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26f16ed15dc402f197a4a7ecb8ac60cd",
+    "url": "/main/index.html"
+  },
+  {
+    "revision": "c61e5ee43c321d9671c4",
+    "url": "/main/static/css/main.3c0136e2.chunk.css"
+  },
+  {
+    "revision": "ed784c8c63f0d03b69e1",
+    "url": "/main/static/js/2.3212ad1e.chunk.js"
+  },
+  {
+    "revision": "c61e5ee43c321d9671c4",
+    "url": "/main/static/js/main.8886be70.chunk.js"
+  },
+  {
+    "revision": "e14f30efc9cc1c5986d8",
+    "url": "/main/static/js/runtime~main.5cf05f57.js"
+  },
+  {
+    "revision": "8a155d4a11e06a284cd0eb41cbd4c439",
+    "url": "/main/static/media/Fuel.8a155d4a.png"
+  },
+  {
+    "revision": "a7709fcce517da6ef293d54ca56b5402",
+    "url": "/main/static/media/mario.a7709fcc.png"
+  },
+  {
+    "revision": "dcd81d56dec6e66f4b11386a9d86e7f1",
+    "url": "/main/static/media/solitaire.dcd81d56.png"
+  },
+  {
+    "revision": "9c750c96a2289107c3861f7dfcf276b0",
+    "url": "/main/static/media/sudoku.9c750c96.png"
+  }
+]);
